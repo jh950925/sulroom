1b0a47a50e2dd2719019d4ada5b3844b2e21e81f
jh950925 <jh950925@naver.com>
2024-07-11 10:29:54 +0900

    swagger 사용을 위한 Response Code 어노테이션 생성

---
+++ src/main/java/config/annotation/ResponseApiCode.java
@@ -0,0 +1,24 @@
+package config.annotation;
+
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * swagger annotation
+ * @since 2024.07.11
+ */
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@ApiResponses({
+        @ApiResponse(responseCode = "200", description = "OK"),
+        @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+        @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+        @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+})
+public @interface ResponseApiCode {
+}
---
+++ src/main/java/config/swagger/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package config.swagger;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * swagger Bean
+ */
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("혼술 커뮤니티")            // 제목
+                .description("개인용 포트폴리오")   // 설명
+                .version("0.1.0");               // 버전
+    }
+}
