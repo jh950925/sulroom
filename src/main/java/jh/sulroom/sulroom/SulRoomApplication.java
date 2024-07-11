383ca608a76450609e203a69c148286c5682f723
jh950925 <jh950925@naver.com>
2024-07-11 09:44:49 +0900

    프로젝트 초기 생성

---
+++ src/main/java/jh/sulroom/sulroom/SulRoomApplication.java
@@ -0,0 +1,13 @@
+package jh.sulroom.sulroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SulRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SulRoomApplication.class, args);
+    }
+
+}
