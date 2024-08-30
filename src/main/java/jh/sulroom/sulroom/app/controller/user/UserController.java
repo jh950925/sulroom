c99f3fdf5e5a1dcbcbabc23a491df38a265c3efb
jh950925 <jh950925@naver.com>
2024-08-30 20:09:39 +0900

    user 화면 이동 컨트롤러 생성

--- src/main/java/jh/sulroom/sulroom/app/controller/user/UserController.java
+++ src/main/java/jh/sulroom/sulroom/app/controller/user/UserController.java
@@ -1,4 +1,29 @@
 package jh.sulroom.sulroom.app.controller.user;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/user")
+@Controller
 public class UserController {
+
+    @GetMapping("/add/view")
+    public String mvUSerAddView() {
+        return "";
+    }
+
+    @GetMapping("/search/id/view")
+    public String mvUSerSearchIdView() {
+        return null;
+    }
+
+    @GetMapping("/search/pass/view")
+    public String mvUserSearchPassView() {
+        return null;
+    }
 }
