b9249f0ff051a6da34157f63988e065586a4b45c
jh950925 <jh950925@naver.com>
2024-08-30 20:34:09 +0900

    user 화면 이동 서비스 초기 생성 생성

--- src/main/java/jh/sulroom/sulroom/app/service/user/UserService.java
+++ src/main/java/jh/sulroom/sulroom/app/service/user/UserService.java
@@ -5,19 +5,18 @@ import jh.sulroom.sulroom.app.model.dto.user.UserVo;
 public interface UserService {
 
     // 회원가입
-    UserVo signup(UserVo userVo);
+    UserVo userAdd(UserVo userVo);
 
     // ID 찾기
-    String findId(UserVo userVo);
+    UserVo userSearchId(UserVo userVo);
 
     // 비밀번호 찾기
-    String findPassword(UserVo userVo);
+    UserVo userSearchPass(UserVo userVo);
 
     // 로그인
-    String login(UserVo userVo);
+    UserVo login(UserVo userVo);
 
     // 로그아웃
     void logout(UserVo userVo);
 
-    UserVo modifyUser(UserVo userVo);
 }
--- src/main/java/jh/sulroom/sulroom/app/service/user/UserServiceImpl.java
+++ src/main/java/jh/sulroom/sulroom/app/service/user/UserServiceImpl.java
@@ -1,93 +1,61 @@
 package jh.sulroom.sulroom.app.service.user;
 
-import jh.sulroom.sulroom.app.mapper.user.UserMapper;
 import jh.sulroom.sulroom.app.model.dto.user.UserVo;
-import jh.sulroom.sulroom.app.model.entity.user.UserEntity;
-import jh.sulroom.sulroom.config.http.ApiResponseVo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
-public class UserServiceImpl implements UserService{
-
-    private ApiResponseVo apiResponseVo;
-    private UserMapper userMapper;
+public class UserServiceImpl implements UserService {
 
 
     /**
-     * 회원 가입
-     * @param userVo 
+     * 회원가입
+     * @param userVo
      * @return UserVo
      */
     @Override
-    public UserVo signup(UserVo userVo) {
-        UserEntity entity = new UserEntity();
+    public UserVo userAdd(UserVo userVo) {
 
-        entity.setUserId(userVo.getUserId());
-        entity.setUserPassword(userVo.getPassword());
-        entity.setUserName(userVo.getUserName());
-        entity.setUserAge(userVo.getUserAge());
-
-        int result = userMapper.insertUser(entity);
-
-        System.out.println("result = " + result);
-
-        return result > 0 ?  userVo : null;
+        return null;
     }
 
     /**
-     * 아이다 찾기
-     * @param userVo 
-     * @return String
-     */
-    @Override
-    public String findId(UserVo userVo) {
-
-        UserVo userVO = new UserVo();
-        
-        // 이름, 생년 월일, 이메일 메일
-        
-        return "";
-    }
-
-    /**
-     * 비밀번호 찾기
+     * 회원 아이디 찾기
      * @param userVo
-     * @return String
+     * @return UserVo
      */
     @Override
-    public String findPassword(UserVo userVo) {
-        return "";
+    public UserVo userSearchId(UserVo userVo) {
+        return null;
     }
 
     /**
-     * 회원정보 수정
-     * @param userVo 
-     * @return
+     * 회원 비밀번호 찾기
+     * @param userVo
+     * @return UserVo
      */
     @Override
-    public UserVo modifyUser(UserVo userVo) {
+    public UserVo userSearchPass(UserVo userVo) {
         return null;
     }
 
     /**
      * 로그인
-     * @param userVo 
-     * @return String
+     * @param userVo
+     * @return UserVo
      */
     @Override
-    public String login(UserVo userVo) {
-        return "";
+    public UserVo login(UserVo userVo) {
+        return null;
     }
 
     /**
      * 로그아웃
-     * @param userVo 
+     * @param userVo
      */
     @Override
     public void logout(UserVo userVo) {
 
     }
-
 }
